29d2f97cf487df348a294b7054ae5705a145d00f
Carlos Olivares <carlosolivares.331@outlook.com>
2022-08-18 12:39:42 -0700

    Beginning Refactoring of Matching Algorithm
    I have created a file to hold the code for matching the scans together.
    I am struggling with how to best refactor this code. What are the best
    representations to use??? Object oriented?

---
+++ code/DataPreparationModule/matching.py
@@ -0,0 +1,56 @@
+"""
+Module Level Docstring
+"""
+
+import os
+import sys
+import shutil
+import pandas as pd
+
+def get_path_to_pid_dicoms(path2pid):
+    """Finds where in the PID folder the dicoms are."""
+    lst_of_nonempty_dirs = []
+    for root, dirs, files in os.walk(path2pid):
+        if not files:
+            continue
+        else:
+            lst_of_nonempty_dirs.append(root)
+    assert lst_of_nonempty_dirs, 'More than one non-empyt subdirectory'
+    return lst_of_nonempty_dirs[0]
+
+def get_dicom_files_for(path2pid):
+    """Returns list of dicom files for a PID path"""
+    path_2_dicoms = get_path_to_pid_dicoms(path2pid)
+    return [file for file in os.listdir(path_2_dicoms) if not ('._' in file) ]
+
+def get_year_from(year_folder):
+    """
+    """
+
+
+class PidHandler():
+    """
+    """
+
+    def __init__(self, root, pid):
+        """
+        """
+        self.pid = pid
+        self.root = root
+        self.path_to_dicoms = get_path_pid_dicoms(self.root)
+        self.dicoms = get_dicom_files_for(self.root)
+
+    def get_year_dict(self):
+        """
+        """
+        year_directories = [
+                folder for folder in os.listdir(self.root) if folder[0] != '.'
+                ]
+        assert len(year_directories) > 0, ('No year folders detected for',
+                'pid {}'.format(pid))
+
+
+    def get_slice(slice_num):
+        """
+        """
+
