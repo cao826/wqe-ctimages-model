41c85eb0cfc569e46b360c16e8760839e0d96706
Carlos Olivares <carlosolivares.331@outlook.com>
2022-08-19 14:21:33 -0700

    Many updates:
    1. Added code for dataset.
    
    2. Added a callable object that noirmalizes the image dataset. It can be
       called as a transform in for the datset code.

---
+++ code/ModelingModule/dataset.py
@@ -0,0 +1,126 @@
+import os
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from PIL import Image, ImageDraw
+import random
+import numpy as np
+
+#fix random seed
+torch.manual_seed(0)
+
+def get_label(dataframe, filename):
+    """
+    Gets the value from the dataframe
+
+    THIS FUNCTION ASSUMES: 
+    - there is a column named 'Filename' where the filname is
+    - there is a column named 'Label' where the label is
+
+    I could write a more general function, but it would be a pain
+    """
+    label = dataframe[dataframe.Filename == filename].Label.values[0]
+    #print(label)
+    return label
+
+def get_tensor(array):
+        """
+        """
+        tensor = torch.tensor(array)
+        tensor = tensor.unsqueeze(0)
+        ## concatenate into three channels
+        tensor = torch.cat([tensor for i in range(3)], dim=0).double()
+        return tensor.float()
+
+def get_pid(filename):
+    """
+    """
+    return filename.split('-')[0]
+
+class NlstDataset(Dataset):
+    """
+    """
+    def __init__(self, path2clinical_data, path2images, neg_subfolder, pos_subfolder, transforms):
+        self.clinical_data = pd.read_csv(path2clinical_data)
+        self.path2images = path2images
+        self.subfolders = [neg_subfolder, pos_subfolder]
+        self.label_to_subfolder = {
+            1: pos_subfolder,
+            0: neg_subfolder
+        }
+        
+        self.pos_files = [file for file in os.listdir(os.path.join(
+            path2images,
+            pos_subfolder))
+            if '.npy' in file]
+
+        self.neg_files = [file for file in os.listdir(os.path.join(
+            path2images,
+            neg_subfolder))
+            if '.npy' in file]
+
+        self.files = self.neg_files + self.pos_files
+        random.shuffle(self.files)
+        print('dataset has {} files'.format(len(self.files)))
+        self.transformations = transforms
+
+    def __len__(self):
+        return len(self.files)
+
+    def get_label(self, filename):
+        """
+        """
+        label = 0
+        if filename in self.pos_files:
+            label = 1
+        return label
+
+    def get_clinical_info_vector(self, pid):
+        """
+        """
+        mask = (self.clinical_data.pid == pid)
+        pid_row = self.clinical_data[mask]
+        assert(pid_row.shape[0] == 1)
+        clinical_info_vec = pid_row[[
+            'race',
+            'cigsmok',
+            'gender',
+            'age'
+        ]].values[0].astype(float)
+        clinical_info_tensor = torch.tensor(clinical_info_vec)
+        #print(clinical_info_vec)
+        #print(clinical_info_tensor)
+        return clinical_info_tensor.float()
+
+    
+    def get_path_to_filename(self, filename, label):
+        """
+        """
+        subfolder = self.label_to_subfolder[label]
+        intermediate = os.path.join(subfolder, filename)
+        path2filename = os.path.join(self.path2images, intermediate)
+        return path2filename
+
+
+    def __getitem__(self, idx):
+        """
+        """
+        filename = self.files[idx]
+        pid = get_pid(filename)
+        label = self.get_label(filename)
+        #print(filename)
+        path2image = self.get_path_to_filename( filename, label)
+        #print(path2image)
+        #print(path2image)
+        image = np.load(path2image)
+        image = self.transformations(image)
+        image = get_tensor(array=image)
+        #image = self.transformations(image)
+        clinical_info = self.get_clinical_info_vector(int(pid))
+        #label = self.labels[label]
+
+        return image, clinical_info, label
+
+if __name__ == 'main':
+    print('Why are you running this a script? This is a module!")
--- code/ModelingModule/transforms.py
+++ code/ModelingModule/transforms.py
@@ -6,7 +6,11 @@ import os
 import sys
 import numpy as np
 from skimage import transform
+import pickle
 
+def detect_sample_shape(sample):
+    """Returns how many dimensions the sample is"""
+    return len(sample.shape)
 
 class Rescale():
     """Rescale the image in a sample to a given size.
@@ -21,16 +25,45 @@ class Rescale():
         assert isinstance(output_size, (int, tuple))
         if isinstance(output_size, int):
             #print('output_size is an int')
-            self.output_size = (output_size, output_size)
+            self.output_shape = (output_size, output_size)
         elif isinstance(output_size, (tuple)):
-            self.output_size = output_size
+            self.output_shape = output_size
         else:
-            raise Exception('outputs size given to Rescale Class is neither int nor tuple')
-        #self.output_size = output_size
+            raise Exception(
+                    ('outputs size given to Rescale Class',
+                    ' is neither int nor tuple'))
 
     def __call__(self, sample):
-        channels, height, width = sample.shape
-        #print('{} channels, height {}, and width {}'.format(channels, height, width))
-        output_shape = (channels, self.output_size[0], self.output_size[1])
-        resized_sample = transform.resize(sample, output_shape, preserve_range=True)
+        #print('Do we even call this function?')
+        #print(f'input_shape: {sample.shape}')
+        sample_dimensionality = detect_sample_shape(sample)
+        if sample_dimensionality == 3:
+            channels, height, width = sample.shape
+            output_shape = (channels,
+                            self.output_shape[0],
+                            self.output_shape[1])
+            resized_sample = transform.resize(image=sample,
+                                              output_shape=output_shape,
+                                              preserve_range=True)
+        elif sample_dimensionality == 2:
+            #print('Two dimensional case')
+            resized_sample = transform.resize(image=sample,
+                                              output_shape=self.output_shape,
+                                              preserve_range=True
+                                             )
+            #print(f'resized_shape: {resized_sample.shape}')
+        else:
+            raise Exception('Sample shape is not recognized')
         return resized_sample
+
+class Normalize():
+    """Uses the mean pixel to normalize each image"""
+    def __init__(self, path_to_mean_pixel):
+        """Initializer method for Normalize callable"""
+        with open(path_to_mean_pixel, 'rb') as fp:
+            self.mean_pixel = pickle.load(fp)
+        fp.close()
+
+    def __call__(self,array):
+        """subtracts the mean pixel from the array"""
+        return array - self.mean_pixel
