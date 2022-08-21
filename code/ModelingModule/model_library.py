965087c0b4b21af193e5501e1e2c567b34111f1b
Carlos Olivares <carlos.olivares.331@outlook.com>
2022-08-21 14:30:00 -0400

    Refactoring model library
    I am adding some function that will make the code better to read.
    There are a lot of magic numbers present in the current code.
    I also think adding some helper functions at the very top will help with defining more model types insted of trying (relatively fruitlessly) to improve with the resnet101 based backbone model. (overfitting like crazy)

--- code/ModelingModule/model_library.py
+++ code/ModelingModule/model_library.py
@@ -1,41 +1,58 @@
+"""
+Module Level Docstring
+"""
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from  torch import nn
+from torch.nn import functional as F
+
+
+def forward_through(linear_layer, activation, input_tensor):
+    """Applies the layer and the activation to the input tensor"""
+    return activation(linear_layer(input_tensor))
 
 class NlstModel(nn.Module):
-    """
+    """First version of model that I designed.
+
+    I have to try this with other backbones.
+    At the very least, a model with less parameters.
+    This model is overfitting like crazy.
     """
     def __init__(self):
         """
         """
-        super().__init__()
+        super().__init__(use_leaky_relu=False)
         self.backbone = torch.hub.load(
             repo_or_dir='pytorch/vision:v0.10.0',
             model='resnet101',
             pretrained=True
         )
         self.get_number_backbone_params()
+
+        if use_leaky_relu:
+            self.change_to_leaky_relu()
+
+        self.linear_1 = nn.Linear(in_features=1004, #this is a magic number
+                                  out_features=100  #these are all magic numbers :(
+)
+        self.linear_2 = nn.Linear(in_features=100,
+                                  out_features=10)
+
+        self.linear_3 = nn.Linear(in_features=10,
+                                  out_features=2)
+
+        self.dropout = nn.Dropout(p=.5)
+
+    def change_to_leaky_relu(self):
+        """Changes the activation functions to LReLU"""
         ### change ReLU to leakyReLU ###
-        for name, module in self.backbone.named_modules():
+        for _, module in self.backbone.named_modules():
             if hasattr(module, 'relu'):
                 module.relu = nn.LeakyReLU(inplace=True)
-        self.fc1 = nn.Linear(
-            in_features=1004,
-            out_features=100
-        )
-        self.fc2 = nn.Linear(
-            in_features=100,
-            out_features=10
-        )
-        self.fc_out = nn.Linear(
-            in_features=10,
-            out_features=2
-        )
-        self.dropout = nn.Dropout(p=.5)
+
     def get_number_backbone_params(self):
         """Returns the number of params the backbone has"""
         count = 0
-        for param in self.backbone.parameters():
+        for _ in self.backbone.parameters():
             count += 1
         self.backbone_params_number = count
 
@@ -43,24 +60,23 @@ class NlstModel(nn.Module):
         """Freezes all params except the last N layers"""
         if freeze_all:
             for param in self.backbone.parameters():
-                param.requires_gradd = False
+                param.requires_grad = False
         too_large_error = 'More params to unfreeze than number of parameters'
         assert last_n_layers < self.backbone_params_number, too_large_error
         unfreeze_threshold = self.backbone_params_number - last_n_layers
         for i, param in enumerate(self.backbone.parameters()):
             if i >= unfreeze_threshold:
                 continue
-            else:
-                param.requires_grad = False
+            param.requires_grad = False
 
     def forward(self, x, clinical_info):
-        """
-        """
+        """Function where the model predicts classes"""
         image_feature_map = self.backbone(x)
-        combined_feature_tensor = torch.cat((clinical_info, image_feature_map), dim=1)
+        combined_feature_tensor = torch.cat((clinical_info, image_feature_map),
+                                            dim=1)
 
-        x = self.dropout(F.leaky_relu(self.fc1(combined_feature_tensor)))
-        x = self.dropout(F.leaky_relu(self.fc2(x)))
-        x = self.fc_out(x)
+        x = self.dropout(F.leaky_relu(self.linear_1(combined_feature_tensor)))
+        x = self.dropout(F.leaky_relu(self.linear_2(x)))
+        x = self.linear_3(x)
 
         return x
