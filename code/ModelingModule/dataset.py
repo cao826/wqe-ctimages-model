ffeb29717c5418cfb646dbaa328405e7ec0ad94e
Carlos Olivares <carlosolivares.331@outlook.com>
2022-08-24 13:22:22 -0700

    using torch.stack instead of torch.cat

--- code/ModelingModule/dataset.py
+++ code/ModelingModule/dataset.py
@@ -27,7 +27,7 @@ def read_as_tensor(path2image):
     """Reads a stored array as a PyTorch tensor"""
     one_channel_tensor = torch.tensor(np.load(path2image))
     print(f"one channel tensor shape: {one_channel_tensor.shape}")
-    three_channel_tensor = torch.cat([
+    three_channel_tensor = torch.stack([
         one_channel_tensor for i in range(3)], dim=0).double()
     print(f"three channel shape: {three_channel_tensor.shape}")
     return three_channel_tensor
