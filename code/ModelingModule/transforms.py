1eda604d74c37e4ae70f85f59fc2e9b7550a60e5
Carlos Olivares <carlosolivares.331@outlook.com>
2022-08-24 12:49:21 -0700

    Removing unnecessary transformations in module
    Now that I know that pytorch supports resizing images with its native
    transforms, I can delete the code to my resizer. I am not fond of my
    code, and have no issue deleting code that is no longer useful. Take
    note, potential employers!

--- code/ModelingModule/transforms.py
+++ code/ModelingModule/transforms.py
@@ -2,67 +2,16 @@
 Module level docstring
 
 """
-import os
-import sys
-import numpy as np
-from skimage import transform
 import pickle
+import torch
 
-def detect_sample_shape(sample):
-    """Returns how many dimensions the sample is"""
-    return len(sample.shape)
-
-class Rescale():
-    """Rescale the image in a sample to a given size.
-
-    Args:
-        output_size (tuple or int): Desired output size. If tuple, output is
-            matched to output_size. If int, smaller of image edges is matched
-            to output_size keeping aspect ratio the same.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        if isinstance(output_size, int):
-            #print('output_size is an int')
-            self.output_shape = (output_size, output_size)
-        elif isinstance(output_size, (tuple)):
-            self.output_shape = output_size
-        else:
-            raise Exception(
-                    ('outputs size given to Rescale Class',
-                    ' is neither int nor tuple'))
-
-    def __call__(self, sample):
-        #print('Do we even call this function?')
-        #print(f'input_shape: {sample.shape}')
-        sample_dimensionality = detect_sample_shape(sample)
-        if sample_dimensionality == 3:
-            channels, height, width = sample.shape
-            output_shape = (channels,
-                            self.output_shape[0],
-                            self.output_shape[1])
-            resized_sample = transform.resize(image=sample,
-                                              output_shape=output_shape,
-                                              preserve_range=True)
-        elif sample_dimensionality == 2:
-            #print('Two dimensional case')
-            resized_sample = transform.resize(image=sample,
-                                              output_shape=self.output_shape,
-                                              preserve_range=True
-                                             )
-            #print(f'resized_shape: {resized_sample.shape}')
-        else:
-            raise Exception('Sample shape is not recognized')
-        return resized_sample
-
-class Normalize():
+class SubtractMeanPixel():
     """Uses the mean pixel to normalize each image"""
     def __init__(self, path_to_mean_pixel):
         """Initializer method for Normalize callable"""
-        with open(path_to_mean_pixel, 'rb') as fp:
-            self.mean_pixel = pickle.load(fp)
-        fp.close()
+        with open(path_to_mean_pixel, 'rb') as file_object:
+            self.mean_pixel = torch.tensor(pickle.load(file_object))
+        file_object.close()
 
     def __call__(self,array):
         """subtracts the mean pixel from the array"""
