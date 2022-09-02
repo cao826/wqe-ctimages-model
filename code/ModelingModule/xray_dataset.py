04435ff602f741e67cc7d203de9827979993346d
Carlos Olivares <carlos.olivares.331@outlook.com>
2022-09-02 14:17:10 -0700

    fixing typo

--- code/ModelingModule/xray_dataset.py
+++ code/ModelingModule/xray_dataset.py
@@ -78,7 +78,7 @@ class Chex8Dataset(Dataset):
         clinical_info_tensor = self.get_clinical_info(filename)
         label = self.get_label(filename)
         image = read_image(os.path.join(self.path2images, filename))
-        image = self.transforms(image)
+        image = self.transformations(image)
         return image, clinical_info, label
 
 if __name__ == 'main':
